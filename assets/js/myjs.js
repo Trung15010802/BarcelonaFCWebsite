9ece1103bf2d1568a5d4d289f685a6299ae79619
Trung Bùi <handsomezezo@gmail.com>
2022-10-13 12:46:00 +0700

    Fix bug

--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -1,6 +1,9 @@
-const navLinks = document.querySelectorAll('.nav-item')
-const menuToggle = document.getElementById('collapsibleNavbar')
-const bsCollapse = new bootstrap.Collapse(menuToggle)
-navLinks.forEach((l) => {
-    l.addEventListener('click', () => { bsCollapse.toggle() })
-})
+var x = window.matchMedia("(max-width: 768px)")
+if (x.matches) {
+    const navLinks = document.querySelectorAll('.nav-item')
+    const menuToggle = document.getElementById('collapsibleNavbar')
+    const bsCollapse = new bootstrap.Collapse(menuToggle)
+    navLinks.forEach((l) => {
+        l.addEventListener('click', () => { bsCollapse.toggle() })
+    })
+}
